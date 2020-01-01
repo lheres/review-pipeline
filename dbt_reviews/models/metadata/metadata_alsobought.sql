c32a03245f2b88a509ba550111deeda0c39d7489
Louwe Heres <louweheres@gmail.com>
2020-01-01 16:24:42 +0100

    add index and update notebook

--- dbt_reviews/models/metadata/metadata_alsobought.sql
+++ dbt_reviews/models/metadata/metadata_alsobought.sql
@@ -1,3 +1,8 @@
+{{ config({
+	"materialized" : "table",
+	"post-hook" : "CREATE INDEX IF NOT EXISTS idx_{{ this.name }}_asin ON {{ this }} USING BTREE (asin);
+					CREATE INDEX IF NOT EXISTS idx_{{ this.name }}_also_bought ON {{ this }} USING BTREE (also_bought);"
+	}) }}
 SELECT raw_json ->> 'asin'::text AS asin,
     jsonb_array_elements(raw_json -> 'related' -> 'also_bought')::text as also_bought
 FROM {{ ref('metadata_raw') }}
--- dbt_reviews/models/metadata/metadata_alsoviewed.sql
+++ dbt_reviews/models/metadata/metadata_alsoviewed.sql
@@ -1,3 +1,8 @@
+{{ config({
+	"materialized" : "table",
+	"post-hook" : "CREATE INDEX IF NOT EXISTS idx_{{ this.name }}_asin ON {{ this }} USING BTREE (asin);
+					CREATE INDEX IF NOT EXISTS idx_{{ this.name }}_also_viewed ON {{ this }} USING BTREE (also_viewed);"
+	}) }}
 SELECT raw_json ->> 'asin'::text AS asin,
 	jsonb_array_elements(raw_json -> 'related' -> 'also_viewed')::text as also_viewed
 FROM {{ ref('metadata_raw') }}
